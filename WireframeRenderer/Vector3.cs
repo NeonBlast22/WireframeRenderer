716f19d4f1c5f86480bfee63ef779c03ad0d47c6
NeonBlast22 <wal@darthsmith.com>
2026-02-16 09:51:25 +1300

    added vector3 struct

---
+++ WireframeRenderer/Vector3.cs
@@ -0,0 +1,45 @@
+namespace WireframeRenderer;
+
+public struct Vector3
+{
+    public float x;
+    public float y;
+    public float z;
+    
+    public Vector3(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3 Normalize()
+    {
+        return this / Magnitude();
+    }
+
+    public float Magnitude()
+    {
+        return MathF.Sqrt(x * x + y * y + z * z);
+    }
+
+    public static Vector3 operator +(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static Vector3 operator -(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static Vector3 operator *(Vector3 a, float b)
+    {
+        return new Vector3(a.x * b, a.y * b, a.z * b);
+    }
+
+    public static Vector3 operator /(Vector3 a, float b)
+    {
+        return new Vector3(a.x / b, a.y / b, a.z / b);
+    }
+}
